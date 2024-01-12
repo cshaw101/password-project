cc4a89c3d0b747a04c02f313d17d5d6e2dad43e6
cshaw101 <shawcaleb1688@gmail.com>
2024-01-12 01:12:19 -0500

    test

--- frontend/password-app/src/App.js
+++ frontend/password-app/src/App.js
@@ -1,28 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 
 //when trying to start application. make sure cd into correct file....different levels of password-app to get to frontend
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <h1>test</h1>
+    </>
+  )
 }
 
 export default App;
