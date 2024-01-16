60564f1b52fac584d4d39b24452799b724efdc3a
cshaw101 <shawcaleb1688@gmail.com>
2024-01-16 05:05:01 -0500

    login page is styled

--- password-app/src/components/loginForm.js
+++ password-app/src/components/loginForm.js
@@ -2,16 +2,12 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './loginForm.css'
 
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [isUsernameFocused, setIsUsernameFocused] = useState(false);
-  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
   const navigate = useNavigate();
 
   const handleLogin = async () => {
@@ -21,15 +17,15 @@ const Login = () => {
         username,
         password,
       });
-  
+
       if (response.data) {
         console.log('Login successful:', response.data);
-      
+
         // Save token, userId, and message in local storage
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('userId', response.data.userId);
         localStorage.setItem('loginMessage', 'Login successful! Welcome.');
-      
+
         // Handle successful login
         navigate('/main');
       } else {
@@ -37,75 +33,72 @@ const Login = () => {
         // Save error message in local storage for displaying on MainPage
         localStorage.setItem('loginError', response.data.message);
         // Handle failed login
-      };
+      }
     } catch (error) {
       console.error('Error during login:', error);
     }
   };
 
+  const handleRegister = async () => {
+    try {
+      console.log('Sending register request...');
+      const response = await axios.post('http://localhost:9000/api/users/register', {
+        username,
+        password,
+      });
 
- const handleRegister = async () => {
-  try {
-    console.log('Sending register request...');
-    const response = await axios.post('http://localhost:9000/api/users/register', {
-      username,
-      password,
-    });
-
-    if (response.data) {
-      console.log('Registration successful:', response.data);
-      // Handle successful registration, 
-    } else {
-      console.error('Registration failed:', response.data.message);
-      // Handle failed registration,
+      if (response.data) {
+        console.log('Registration successful:', response.data);
+        // Handle successful registration,
+      } else {
+        console.error('Registration failed:', response.data.message);
+        // Handle failed registration,
+      }
+    } catch (error) {
+      console.error('Error during registration:', error);
+      // Handle network or other errors
     }
-  } catch (error) {
-    console.error('Error during registration:', error);
-    // Handle network or other errors
-  }
-};
+  };
 
-return (
-  <div className="card-container">
-    <div className="card">
-      <h2>Login</h2>
-      <Form>
-        <FloatingLabel
-          controlId="formUsername"
-          label={isUsernameFocused || username ? '' : 'Username'}
-        >
+  return (
+    <div className="body">
+    <div className="card-container">
+      <div className="card">
+        <h2 className="title">LOGIN</h2>
+        <div>
+        <Form.Label className="username-text" htmlFor="inputUsername">Username</Form.Label>
           <Form.Control
             type="text"
+            id="inputUsername"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            onFocus={() => setIsUsernameFocused(true)}
-            onBlur={() => setIsUsernameFocused(false)}
           />
-        </FloatingLabel>
-        <FloatingLabel
-          controlId="formPassword"
-          label={isPasswordFocused || password ? '' : 'Password'}
-        >
+          <Form.Label className="password-text"  htmlFor="inputPassword">Password</Form.Label>
+        </div>
+        <div>
           <Form.Control
             type="password"
+            id="inputPassword"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            onFocus={() => setIsPasswordFocused(true)}
-            onBlur={() => setIsPasswordFocused(false)}
           />
-        </FloatingLabel>
+             <Form.Text id="passwordHelpBlock" muted>
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+        </Form.Text>
+        </div>
         <div className="button-container">
-          <Button variant="primary" size="lg" onClick={handleLogin}>
-            Login
-          </Button>
-          <Button variant="secondary" size="lg" onClick={handleRegister}>
+        <Button className="login-button" variant="primary" size="lg" onClick={handleLogin}active>
+        Login
+      </Button>
+          <Button className="register-button" variant="secondary" size="lg" onClick={handleRegister}active>
             Register
           </Button>
         </div>
-      </Form>
+      </div>
     </div>
-  </div>
-);
+    </div>
+  );
 };
 
 export default Login;
